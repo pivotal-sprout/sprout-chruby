e81c3045c6ac59eeffa893a19208ed179f1c0cc5
Dan Wendorf <dwendorf@pivotal.io>
2015-10-03 19:07:28 -0700

    Install Ruby 2.2.3 by default
    Also set it as default Ruby
    
    We can now install Ruby 2.2.x, so we should, since it's the latest.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,12 @@
 node.default['sprout']['chruby']['auto_change_ruby'] = true
-node.default['sprout']['chruby']['default_ruby'] = 'ruby-2.1.7'
+node.default['sprout']['chruby']['default_ruby'] = 'ruby-2.2.3'
 
 include_attribute 'sprout-base::home'
 node.default['sprout']['chruby']['rubies_dir'] = File.join(node['sprout']['home'], '.rubies')
 node.default['sprout']['chruby']['rubies'] = {
   'ruby' => [
     '2.0.0-p647',
-    '2.1.7'
+    '2.1.7',
+    '2.2.3'
   ]
 }
