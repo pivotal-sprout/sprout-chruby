eb4589bb6d6bb075cae826a3efadcfaedd9f4a1a
Dan Wendorf <dwendorf@pivotal.io>
2015-10-03 19:03:33 -0700

    Support installing Ruby 2.2.x
    The installer for Rubygems has been changed (see
    https://github.com/ruby/ruby/commit/a7f03a4659d34a459f915a41f260a1b9299ef219)
    to use the GEM_HOME variable if present. We need to unset it before we
    execute ruby-installer

--- recipes/rubies.rb
+++ recipes/rubies.rb
@@ -5,6 +5,7 @@ node['sprout']['chruby']['rubies'].each do |ruby_vm, ruby_versions|
   ruby_versions.each do |ruby_version|
     execute "ruby-install --cleanup --rubies-dir #{ruby_install_dir} #{ruby_vm} #{ruby_version}" do
       user node['sprout']['user']
+      environment('GEM_HOME' => nil)
       not_if { Dir.exist?(File.join(ruby_install_dir, [ruby_vm, ruby_version].join('-'))) }
     end
   end
