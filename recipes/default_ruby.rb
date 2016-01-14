d3bf3aa75b37893808fedd706c2e3048a4da341c
Aram Price <aprice@pivotal.io>
2016-01-13 18:11:03 -0800

    Always setup chruby defaults
    * ensures that changes take effect even if settings files are present
    
    resolves #5

--- recipes/default_ruby.rb
+++ recipes/default_ruby.rb
@@ -4,13 +4,11 @@ if node['sprout']['chruby']['default_ruby']
     file File.join(node['sprout']['home'], '.ruby-version') do
       content node['sprout']['chruby']['default_ruby']
       user node['sprout']['user']
-      action :create_if_missing
     end
   else
     file File.join(node['bash_it']['dir'], 'custom', 'chruby-default_ruby.bash') do
       content "chruby #{node['sprout']['chruby']['default_ruby']}"
       user node['sprout']['user']
-      action :create_if_missing
     end
   end
 end
