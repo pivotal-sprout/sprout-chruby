8e387002d794b45301e1bd56d7f08952cfa2bb11
Dan Wendorf <dwendorf@pivotal.io>
2016-05-08 14:53:43 -0700

    Add source_url and issues_url

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,8 @@
 name             'sprout-chruby'
 maintainer       'Pivotal'
 maintainer_email 'sprout-maintainers@googlegroups.com'
+source_url       'https://github.com/pivotal-sprout/sprout-chruby'
+issues_url       'https://github.com/pivotal-sprout/sprout-chruby/issues'
 license          'MIT'
 description      'Template repo for creating standalone cookbooks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
