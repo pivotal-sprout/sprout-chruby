a319e5abe48de78c0f85d1f44547b8ca04b15360
Alex Slynko <slynko@outlook.com>
2016-06-01 12:28:07 +0100

    Update rubies.rb
    Do not reinstall rubies if they already exist. Easier to parse with the eyes than the `not_if`

--- recipes/rubies.rb
+++ recipes/rubies.rb
@@ -3,10 +3,9 @@ package 'ruby-install'
 ruby_install_dir = node['sprout']['chruby']['rubies_dir']
 node['sprout']['chruby']['rubies'].each do |ruby_vm, ruby_versions|
   ruby_versions.each do |ruby_version|
-    execute "ruby-install --cleanup --rubies-dir #{ruby_install_dir} #{ruby_vm} #{ruby_version}" do
+    execute "ruby-install --cleanup --no-reinstall --rubies-dir #{ruby_install_dir} #{ruby_vm} #{ruby_version}" do
       user node['sprout']['user']
       environment('GEM_HOME' => nil)
-      not_if { Dir.exist?(File.join(ruby_install_dir, [ruby_vm, ruby_version].join('-'))) }
     end
   end
 end
