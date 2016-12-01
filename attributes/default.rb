b9bc0d7619a93609bf874573dc5efcd04d18ee3a
aram price <aprice@pivotal.io>
2016-12-01 12:17:09 -0800

    Change additional default ruby value from 2.3.1 => 2.3.3

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,6 +5,6 @@ include_attribute 'sprout-base::home'
 node.default['sprout']['chruby']['rubies_dir'] = File.join(node['sprout']['home'], '.rubies')
 node.default['sprout']['chruby']['rubies'] = {
   'ruby' => [
-    '2.3.1'
+    '2.3.3'
   ]
 }
