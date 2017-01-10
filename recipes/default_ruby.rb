92abdf8f61b0c71734c818fbc7b4ddb25c279776
aram price <aprice@pivotal.io>
2017-01-10 12:34:03 -0800

    Stop `puts`ing ruby version

--- recipes/default_ruby.rb
+++ recipes/default_ruby.rb
@@ -1,5 +1,4 @@
 if node['sprout']['chruby']['default_ruby']
-  puts "node['sprout']['chruby']['default_ruby'] #{node['sprout']['chruby']['default_ruby']}"
   if node['sprout']['chruby']['auto_change_ruby']
     file File.join(node['sprout']['home'], '.ruby-version') do
       content node['sprout']['chruby']['default_ruby']
