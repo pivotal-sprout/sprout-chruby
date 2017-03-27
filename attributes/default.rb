3d1a9d322370a2348815fe6729e6b89513f39bd6
aram price <aprice@pivotal.io>
2017-03-27 12:00:47 -0700

    Bump default ruby to 2.4.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,10 @@
 node.default['sprout']['chruby']['auto_change_ruby'] = true
-node.default['sprout']['chruby']['default_ruby'] = 'ruby-2.4.0'
+node.default['sprout']['chruby']['default_ruby'] = 'ruby-2.4.1'
 
 include_attribute 'sprout-base::home'
 node.default['sprout']['chruby']['rubies_dir'] = File.join(node['sprout']['home'], '.rubies')
 node.default['sprout']['chruby']['rubies'] = {
   'ruby' => [
-    '2.4.0'
+    '2.4.1'
   ]
 }
--- spec/unit/rubies_spec.rb
+++ spec/unit/rubies_spec.rb
@@ -8,7 +8,7 @@ describe 'sprout-chruby::rubies' do
   it 'installs a default list of rubies' do
     runner.converge(described_recipe)
 
-    expect(runner).to run_execute("#{ruby_install_cmd} ruby 2.4.0")
+    expect(runner).to run_execute("#{ruby_install_cmd} ruby 2.4.1")
   end
 
   it 'installs a specified list of rubies' do
